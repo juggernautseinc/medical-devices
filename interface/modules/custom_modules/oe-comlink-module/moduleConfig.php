f2a46daf1d4d615f81a3410b18ace00bc2f210d2
juggernautsei <sherwin@affordablecustomehr.com>
2022-11-04 20:44:42 -0400

    Refactoring module - adding bootstrap file and namespace

---
+++ interface/modules/custom_modules/oe-comlink-module/moduleConfig.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ *
+ * @package      Comlink OpenEMR
+ * @link               https://www.open-emr.org
+ *
+ * @author    Sherwin Gaddis <sherwingaddis@gmail.com>
+ * @copyright Copyright (c) 2021 Sherwin Gaddis <sherwingaddis@gmail.com>
+ * @license   https://github.com/openemr/openemr/blob/master/LICENSE GNU General Public License 3
+ *
+ */
+
+namespace Comlink\OpenEMR\Module;
+
+$module_config = 1;
+
+require_once('setup.php');
--- interface/modules/custom_modules/oe-comlink-module/openemr.bootstrap.php
+++ interface/modules/custom_modules/oe-comlink-module/openemr.bootstrap.php
@@ -10,12 +10,62 @@
 
 namespace Comlink\OpenEMR\Module;
 
+use OpenEMR\Events\Globals\GlobalsInitializedEvent;
+use OpenEMR\Menu\MenuEvent;
+use OpenEMR\Services\Globals\GlobalSetting;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+
+function comlink_add_menu_item(MenuEvent $event)
+{
+    $menu = $event->getMenu();
+
+    $menuItem = new stdClass();
+    $menuItem->requirement = 0;
+    $menuItem->target = 'fin';
+    $menuItem->menu_id = 'fin0';
+    $menuItem->label = xlt("Patient Monitoring");
+    $menuItem->url = "/interface/modules/custom_modules/oe-comlink-module/comlinkUI.php";
+    $menuItem->children = [];
+    $menuItem->acl_req = ["patients", "docs"];
+    $menuItem->global_req = [];
+
+    foreach ($menu as $item) {
+        if ($item->menu_id == 'patimg') {
+            $item->children[] = $menuItem;
+            break;
+        }
+    }
+
+    $event->setMenu($menu);
+
+    return $event;
+}
+
 /**
- * @global EventDispatcher $eventDispatcher Injected by the OpenEMR module loader;
+ * @var EventDispatcherInterface $eventDispatcher
+ * @var array                    $module
+ * @global                       $eventDispatcher @see ModulesApplication::loadCustomModule
+ * @global                       $module          @see ModulesApplication::loadCustomModule
  */
+$eventDispatcher->addListener(MenuEvent::MENU_UPDATE, 'comlink_add_menu_item');
 
-//$bootstrap = new Bootstrap($eventDispatcher, $GLOBALS['kernel']);
-//$bootstrap->subscribeToEvents();
+/**
+ * @throws Exception
+ */
+function createFaxModuleGlobals(GlobalsInitializedEvent $event)
+{
+    $instructuname = xl('Enter username from comlink account.');
+    $instructupass = xl('Enter password from comlink account.');
+    $instructorgid = xl('Enter organizaion identification.');
+    $event->getGlobalsService()->createSection("Comlink Device Module", "Billing");
+    $setting = new GlobalSetting(xl('Comlink Username'), 'text', '', $instructuname);
+    $event->getGlobalsService()->appendToSection("Comlink Device Module", "comlink_username", $setting);
+    $setting = new GlobalSetting(xl('Comlink Password'), 'encrypted', '', $instructupass);
+    $event->getGlobalsService()->appendToSection("Comlink Device Module", "comlink_password", $setting);
+    $setting = new GlobalSetting(xl('Comlink Org ID'), 'text', '', $instructorgid);
+    $event->getGlobalsService()->appendToSection("Comlink Device Module", "comlink_xorgid", $setting);
+}
 
-
+$eventDispatcher->addListener(GlobalsInitializedEvent::EVENT_HANDLE, 'createFaxModuleGlobals');
 
